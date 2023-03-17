38d86a5d0d57f9bb38af543c5aa4a05828914e99
chris <5sensprod@gmail.com>
2023-03-17 19:00:44 +0100

    ajout de la fonction qui pins les critères

--- scripts/factories/dropdownFactory.js
+++ scripts/factories/dropdownFactory.js
@@ -1,8 +1,15 @@
+
+import {displayInSearchCriteria} from '../handlers/searchCriteria.js';
 // Factory pour créer un élément li de dropdown avec du texte donné en paramètre
 
 export function createListItem(textContent) {
   const listItem = document.createElement("li");
   listItem.classList.add("dropdown__menu-item");
   listItem.textContent = textContent;
+
+  listItem.addEventListener('click', () => {
+    displayInSearchCriteria(listItem.textContent);
+  });
+
   return listItem;
 }
---
+++ scripts/handlers/searchCriteria.js
@@ -0,0 +1,21 @@
+
+export function displayInSearchCriteria(text) {
+    const searchCriteriaDiv = document.querySelector('.search-criteria');
+    const searchCriteriaItem = document.createElement('span');
+    searchCriteriaItem.classList.add('search-criteria__item');
+    searchCriteriaItem.textContent = text;
+  
+    // Créez l'icône de fermeture et ajoutez la classe Font Awesome
+    const closeButton = document.createElement('i');
+    closeButton.classList.add('fas', 'fa-times', 'search-criteria__close-icon');
+  
+    // Ajoutez un gestionnaire d'événements de clic pour supprimer le critère
+    closeButton.addEventListener('click', () => {
+      searchCriteriaItem.remove();
+    });
+  
+    // Ajoutez l'icône de fermeture au critère
+    searchCriteriaItem.appendChild(closeButton);
+  
+    searchCriteriaDiv.appendChild(searchCriteriaItem);
+  }

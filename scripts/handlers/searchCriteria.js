3a27479266c558dc599007fed859063ac267fc5f
chris <5sensprod@gmail.com>
2023-03-18 14:49:16 +0100

    modif ajout de ul a la place de div

--- scripts/handlers/searchCriteria.js
+++ scripts/handlers/searchCriteria.js
@@ -1,34 +1,45 @@
 export function displayInSearchCriteria(text, listType) {
   const searchCriteriaDiv = document.querySelector('.search-criteria');
+  let searchCriteriaContainer = searchCriteriaDiv.querySelector(`.search-criteria__container--${listType}`);
+  let searchCriteriaList = searchCriteriaDiv.querySelector(`.search-criteria__list--${listType}`);
 
-  // Vérifie si le critère existe déjà dans les critères de recherche
-  const existingCriteria = searchCriteriaDiv.querySelectorAll('.search-criteria__item');
-  const existingContainer = Array.from(existingCriteria).find(
-    (criteriaItem) => criteriaItem.textContent === text && criteriaItem.parentElement.classList.contains(`search-criteria__container--${listType}`)
-  );
-  
-  if (existingContainer) {
+  // Vérifie si la div existante est trouvée
+  if (!searchCriteriaList) {
+    // Si la liste n'existe pas, créer une nouvelle div avec la classe appropriée et la liste
+    searchCriteriaContainer = document.createElement('div');
+    searchCriteriaContainer.classList.add('search-criteria__container');
+    searchCriteriaContainer.classList.add(`search-criteria__container--${listType}`);
+    searchCriteriaDiv.appendChild(searchCriteriaContainer);
+
+    searchCriteriaList = document.createElement('ul');
+    searchCriteriaList.classList.add('search-criteria__list');
+    searchCriteriaList.classList.add(`search-criteria__list--${listType}`);
+    searchCriteriaContainer.appendChild(searchCriteriaList);
+  }
+
+  // Vérifie si le critère existe déjà dans la liste
+  const existingCriteria = searchCriteriaList.querySelectorAll('.search-criteria__item');
+  const existingItem = Array.from(existingCriteria).find(item => item.textContent === text);
+  if (existingItem) {
     // Si le critère existe déjà, ne fait rien
     return;
   }
 
-  // Créer une nouvelle div avec la classe appropriée au critère
-  const searchCriteriaContainer = document.createElement('div');
-  searchCriteriaContainer.classList.add('search-criteria__container');
-  searchCriteriaContainer.classList.add(`search-criteria__container--${listType}`);
-  searchCriteriaDiv.appendChild(searchCriteriaContainer);
-
-  const searchCriteriaItem = document.createElement('span');
+  // Sinon, ajouter simplement le critère à l'intérieur de la liste
+  const searchCriteriaItem = document.createElement('li');
   searchCriteriaItem.classList.add('search-criteria__item');
   searchCriteriaItem.textContent = text;
-  searchCriteriaContainer.appendChild(searchCriteriaItem);
+  searchCriteriaList.appendChild(searchCriteriaItem);
 
   const closeButton = document.createElement('img');
   closeButton.src = 'img/icon-close.svg';
   closeButton.alt = 'close icon';
   closeButton.classList.add('search-criteria__close-icon');
   closeButton.addEventListener('click', () => {
-    searchCriteriaContainer.remove();
+    searchCriteriaItem.remove();
+    if (searchCriteriaList.querySelectorAll('.search-criteria__item').length === 0) {
+      searchCriteriaContainer.remove();
+    }
   });
-  searchCriteriaContainer.appendChild(closeButton);
+  searchCriteriaItem.appendChild(closeButton);
 }

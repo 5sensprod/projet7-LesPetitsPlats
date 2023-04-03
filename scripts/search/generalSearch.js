0831a2f28bee606f50bab174e6af4fbb00418ee4
chris <5sensprod@gmail.com>
2023-04-04 01:49:36 +0200

    suppression log performance

--- scripts/search/generalSearch.js
+++ scripts/search/generalSearch.js
@@ -14,21 +14,17 @@ function getRecipeCardElementsFromData(filteredRecipesData) {
 
 // Filtre les recettes en utilisant les boucles natives
 export function filterRecipes() {
-  const startTime = performance.now();
   const searchInput = document.getElementById('search-input');
   const query = normalizeString(searchInput.value.trim());
   const searchCriteria = document.querySelectorAll('.search-criteria__item');
   const recipeData = getRecipeData();
   const filteredRecipes = [];
 
-  let operationCount = 0;
-
   let i = 0;
   while (i < recipeData.length) {
     const recipe = recipeData[i];
     const normalizedQuery = normalizeString(query);
     let matchesSearchQuery = query.length < 3 || normalizeString(recipe.name).includes(normalizedQuery) || recipe.ingredients.some(ingredient => normalizeString(ingredient.ingredient).includes(normalizedQuery)) || normalizeString(recipe.description).includes(normalizedQuery);
-    operationCount++; // Opération : vérification de la correspondance avec la recherche principale
 
     let matchesSearchCriteria = true;
     let j = 0;
@@ -41,21 +37,16 @@ export function filterRecipes() {
         matchesSearchCriteria = false;
         break;
       }
-      operationCount++; // Opération : vérification de la correspondance avec un ingrédient
 
       if (listType === 'appliance' && recipe.appliance !== normalizedText) {
         matchesSearchCriteria = false;
         break;
       }
 
-      operationCount++; // Opération : vérification de la correspondance avec
-
       if (listType === 'ustensil' && !recipe.ustensils.some(ustensil => ustensil === normalizedText)) {
         matchesSearchCriteria = false;
         break;
       }
-
-      operationCount++; // Opération : vérification de la correspondance avec un ustensile
 
       j++;
     }
@@ -70,8 +61,4 @@ export function filterRecipes() {
   const filteredRecipeCards = getRecipeCardElementsFromData(filteredRecipes);
   updateRecipeDisplay(false, filteredRecipeCards);
   updateDropdownLists(filteredRecipes);
-  const endTime = performance.now();
-  const duration = endTime - startTime;
-  console.log('Durée d\'exécution :', duration, 'ms');
-  console.log('Nombre d\'opérations :', operationCount);
 }
